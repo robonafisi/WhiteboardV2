d703ea858f4b19f721b43b4c41d9bf58264588ff
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-28 20:02:58 +0800

    The basic API get call

--- FullStack/PracticeArena/client/src/App.js
+++ FullStack/PracticeArena/client/src/App.js
@@ -3,13 +3,23 @@ import axios from 'axios';
 import React, { Component } from 'react'
 
 const api = axios.create({
-  baseURL:""
+  baseURL: `https://opentdb.com/api.php?amount=10`
 })
 
 export default class App extends Component {
+
+  constructor(){
+    super();
+    api.get('/').then(res => {
+      console.log(res.data)
+    })
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        
+      </div>
     )
   }
 }
