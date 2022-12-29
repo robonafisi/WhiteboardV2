05e4aad17164a588aa674832259d6dff4e4a5cc4
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-30 00:41:59 +0800

    The initial rectangle drawing

---
+++ WhiteboardV2/client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DrawRectangle from './components/drawrectangle/drawrectangle.js'
+
+export default function App() {
+  return (
+    <div>
+      <DrawRectangle />
+    </div>
+  )
+}
