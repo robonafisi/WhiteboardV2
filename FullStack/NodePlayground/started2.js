8f2a5e6a930793e499f574cc17cc631be364b35e
Mohammad Nafisi <nafisidev@gmail.com>
2022-12-31 20:42:09 +0800

    Other ways to import

--- FullStack/NodePlayground/started2.js
+++ FullStack/NodePlayground/started2.js
@@ -1,3 +1,7 @@
 const names = require('./started');
+const itemlist = require('./started3');
+require('./started4');
 
+
+console.log(itemlist);
 names("ghasim");
---
+++ FullStack/NodePlayground/started3.js
@@ -0,0 +1,8 @@
+module.exports.items = ["item 1", "item 2", "item 3", "item 4", "item 5"]
+
+const person = {
+    name: "Robert",
+    last_name: "Rabiei",
+    role: "Operations"
+}
+
